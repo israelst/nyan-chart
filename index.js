555b80b0599043ce05374717318fbb52ccab6e22
Israel Teixeira <israelst@gmail.com>
2014-12-10 22:41:58 -0200

    Add side by side example

--- index.js
+++ index.js
@@ -2,23 +2,37 @@ var d3 = require('d3'),
     dot = require('./charts/dot').dot;
 
 window.addEventListener('load', function(){
-    var data = [['Category 1', 100],
-                ['Category 2', 87],
-                ['Category 3', 53],
+    var data1 = [['Category 1', 53],
+                ['Category 2', 23],
+                ['Category 3', 100],
                 ['Category 4', 44],
-                ['Category 5', 23]],
+                ['Category 5', 87]],
+        data2 = [['Category 1', 53],
+                ['Category 2', 23],
+                ['Category 3', 100],
+                ['Category 4', 87]],
         colors = [
             '#65ffff',
             '#9966ff',
             '#fecd66',
             '#ff7e66',
             '#e0ff65',
-        ],
-        chart = dot(colors);
+        ];
 
     d3.select('body')
         .append('svg')
-        .attr('id', 'dot')
-        .datum(data)
-        .call(chart);
+        .style('width', '50%')
+        .datum(data1)
+        .call(dot(colors));
+
+    d3.select('body')
+        .append('svg')
+        .style('width', '50%')
+        .datum(data2)
+        .call(dot());
+
+    d3.select('body')
+        .append('svg')
+        .datum(data2)
+        .call(dot());
 });
