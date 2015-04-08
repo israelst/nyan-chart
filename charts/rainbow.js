debf90da634ccdb9e2aefd7081d8b84bbabcc31c
Israel Teixeira <israelst@gmail.com>
2015-04-08 19:21:45 -0300

    Extract filter append from rainbow

--- charts/rainbow.js
+++ charts/rainbow.js
@@ -7,18 +7,21 @@ var d3 = require('d3'),
 
 // TODO: Revisit the constructor, there is too many parameters, mind the default values
 exports.rainbow = function (x, y, color, category, value, data, transitionConfig){
+    function addFilter(selection, id){
+        if(selection.select('#' + id).size() === 0){
+            selection.select('defs')
+                .append('filter').attr('id', id)
+                .append('feGaussianBlur')
+                    .attr('in', 'SourceGraphic')
+                    .attr('stdDeviation', 80);
+        }
+    }
     return function(selection){
         var ry = d3.max(y.range()),
             filterId = 'nyanBlur',
             spots = selection.select('g.spots');
 
-        if(selection.select('#' + filterId).size() === 0){
-            selection.select('defs')
-                .append('filter').attr('id', filterId)
-                .append('feGaussianBlur')
-                    .attr('in', 'SourceGraphic')
-                    .attr('stdDeviation', 80);
-        }
+        addFilter(selection, filterId);
 
         spots.style('filter', 'url(#' + filterId + ')')
             .selectAll('ellipse')
